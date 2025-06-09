d17ba613ab92543faeb9ca8ef3eced7fbe1a88af
Olesya <panna82@mail.ru>
2025-06-10 01:48:14 +0500

    initial

---
+++ HomeTask_9_2.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+void swap_metod(int* a, int* b)
+{
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+
+    std::cout << "a = " << *a << ", ";
+    std::cout << "b = " << *b << std::endl;
+}
+
+
+int main()
+{
+    int a = 5;
+    int b = 8;
+    int* ptra = &a;
+    int* ptrb = &b;
+
+    std::cout << "a = " << a << ", ";
+    std::cout << "b = " << b << std::endl;
+
+    swap_metod(&a, &b);
+}
